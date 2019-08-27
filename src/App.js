d7411980bbf1e8853dea8c7756c7181c3e91e683
trickyBytes <richard.tiffin@kaluza.com>
2019-08-27 09:57:24 +0100

    chapter-3: Using simple state in app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 26}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29">My Hobbies: Reacing</Person>
-        <Person name="Stephanie" age="26" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
