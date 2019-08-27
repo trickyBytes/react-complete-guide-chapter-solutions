7a12c8ca41a2ed22d2ed8a1ee8f89ff35790823f
trickyBytes <richard.tiffin@kaluza.com>
2019-08-27 11:19:21 +0100

    chapter-3: Pass function to component

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler}
         />
         <Person
           name={this.state.persons[2].name}
