c3e3528ad237fd8686e8680c246d27817b2aff15
trickyBytes <richard.tiffin@kaluza.com>
2019-08-27 20:30:42 +0100

    chapter-7: Split into smaller components

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const cockpit = props => {
+  const style = {
+    backgroundColor: "white",
+    font: "inherit",
+    border: "1px solid blue",
+    padding: "8px",
+    cursor: "pointer"
+  };
+
+  return (
+    <div>
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      {/* One way of binding a method call to on click event, use bind primarliy as it's more efficient (not sure why) */}
+      <button style={style} onClick={props.clicked}>
+        Switch Name
+      </button>
+    </div>
+  );
+};
+
+export default cockpit;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import Person from "./Person/Person";
+
+import Cockpit from "../components/Cockpit/Cockpit";
+import Persons from "../components/Persons/Persons";
 import "./App.css";
 
 class App extends Component {
   state = {
     persons: [
-      { id: 'asd', name: "Max", age: 28 },
-      { id: 'asdasdas', name: "Manu", age: 29 },
-      { id: 'weewrewr', name: "Stephanie", age: 26 }
+      { id: "asd", name: "Max", age: 28 },
+      { id: "asdasdas", name: "Manu", age: 29 },
+      { id: "weewrewr", name: "Stephanie", age: 26 }
     ],
     otherState: "Some other Value",
     showPersons: false
@@ -28,13 +30,13 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   };
 
-  deletePersonHander = (personIndex) => {
+  deletePersonHander = personIndex => {
     const persons = [...this.state.persons]; //New array with objects from old - makes state immutable here.
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -43,42 +45,24 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer"
-    };
-
     let persons = null;
 
     if (this.state.showPersons === true) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHander(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-              />
-            );
-          })}
-        </div>
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHander}
+          changed={this.nameChangedHandler}
+        />
       );
     }
 
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        {/* One way of binding a method call to on click event, use bind primarliy as it's more efficient (not sure why) */}
-        <button style={style} onClick={this.togglePersonsHandler}>
-          Switch Name
-        </button>
+        <Cockpit
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
